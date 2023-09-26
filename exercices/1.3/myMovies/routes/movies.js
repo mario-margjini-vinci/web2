a41345c5d10847622c53442f9eb34ced3d6aa518
Mario Margjini <mario.margjini@student.vinci.be>
2023-09-26 12:41:08 +0200

    1.3: not finished

---
+++ exercices/1.3/myMovies/routes/movies.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+const MOVIES = [
+    {
+       id : 1,
+       title: 'The Equalizer 3',
+       duration:  109,
+       budget: 70,
+       link: 'https://www.imdb.com/title/tt17024450/'
+
+    },
+    {
+        id:2,
+        title:'FAST X',
+        duration: 141,
+        budget:340,
+        link:'https://www.imdb.com/title/tt5433140/'
+    },
+    {
+        id:3,
+        title:'Dragon ball super : Broly',
+        duration: 100,
+        budget: 8.5,
+        link: 'https://www.imdb.com/title/tt7961060/'
+    },
+
+];
+
+// Read all the movies from the menu
+router.get('/',(req,res,next) =>{
+    console.log('GET /movies');
+    const minimumFilmDuration = req?.query
+    ? Number(req.query['minimum-duration'])
+    : undefined;
+    if (typeof minimumFilmDuration !=='number' || minimumFilmDuration<=0||!minimumFilmDuration){
+        return res.json(MOVIES);
+    }
+    const  filmsReachingMinimumDuration = MOVIES.filter(
+        (film) => film.duration >= minimumFilmDuration
+    );
+    return res.json(filmsReachingMinimumDuration);
+});
+
+module.exports = router;
